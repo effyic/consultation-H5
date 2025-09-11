e5cd0376808c53553be34512622ebebef593cbd2
dingding6 <dingzhen0729@163.com>
2025-09-11 14:56:05 +0800

    prod加入医院包名

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 IMAGE_TAG=swr.cn-north-4.myhuaweicloud.com/registry-huawei/effyic/consultation-h5:latest
 
 if [ "$1" == "prod" ]; then
-  IMAGE_TAG=swr.cn-north-4.myhuaweicloud.com/registry-huawei/effyic/consultation-h5:latest
+  IMAGE_TAG=swr.cn-north-4.myhuaweicloud.com/registry-huawei/effyic/consultation-h5-hospital:latest
 fi
 
 docker build . --platform linux/amd64 -t $IMAGE_TAG
